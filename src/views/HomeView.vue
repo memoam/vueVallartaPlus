807a7414b97071428b514a8d5323a99a00c96b9d
Guillemo <63140755+memoam@users.noreply.github.com>
2023-05-23 00:01:07 -0600

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <h1>This is an home page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+
+// export default {
+//   name: 'HomeView',
+//   components: {
+//     HelloWorld,
+//   },
+// };
+</script>
